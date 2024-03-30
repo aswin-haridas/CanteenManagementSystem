be4ef27362ea427a8d2d21060856284e2d0073bf
aswin-haridas <aswinharidas0@gmail.com>
2024-03-30 20:49:22 +0530

    commited

--- workarea.py
+++ workarea.py
@@ -2,21 +2,35 @@ import sqlite3
 
 DATABASE = 'canteen.db'
 
+def create_users_table():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT NOT NULL UNIQUE,
+                    role TEXT NOT NULL,
+                    score INTEGER NOT NULL
+                    )''')
+
+    conn.commit()
+    conn.close()
+
 def add_users_with_roles():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
 
-    # Insert users with roles into the users table
     users_with_roles = [
-        (0,"admin", "admin", 100),
-        (1,"canteen_manager", "canteen manager", 100)
-        (2,"manu", "user", 100),  
+        (0, "admin", "admin", 100),
+        (1, "canteen_manager", "canteen manager", 100),
+        (435034, "manu", "user", 100),  
     ]
 
-    cursor.executemany("INSERT INTO users (id , username, role, score) VALUES (?, ?, ?)", users_with_roles)
+    cursor.executemany("INSERT INTO users (id, username, role, score) VALUES (?, ?, ?, ?)", users_with_roles)
 
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
+    create_users_table()
     add_users_with_roles()
