9cddf7e83cba1ac2f64b94b2395e9e19470d3ab9
aswin-haridas <aswinharidas0@gmail.com>
2024-04-02 19:06:51 +0530

    commit

--- app.py
+++ app.py
@@ -2,42 +2,159 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Secret key for session management
+app.secret_key = 'supersecretkey'  
 DATABASE = 'database.db'
+
+@app.context_processor
+def common_icons():
+    icons = {
+        'admin': "/static/assets/admin.png",
+        'customer': "/static/assets/customer.png",
+        'manager': "/static/assets/canteen-manager.png",
+        'editimage': "/static/assets/edit.png"
+    }
+    return icons
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
+@app.route('/home')
 def home():
-    current_user = session.get('user_id', 1)  # Get user_id from session or default to 1
+    current_user = session.get('user_id', 1)  
     conn = get_db_connection()
     menu_cursor = conn.execute('SELECT * FROM Menu')
     menu_items = menu_cursor.fetchall()
-    cart_cursor = conn.execute('SELECT ci.id, mi.name, mi.price, ci.quantity FROM Cart ci JOIN Menu mi ON ci.item_id = mi.id WHERE ci.user_id = ?', (current_user,))
+    cart_cursor = conn.execute('SELECT * FROM Cart')
     cart_items = cart_cursor.fetchall()
     total_price = sum(item['price'] * item['quantity'] for item in cart_items)
     conn.close()
     return render_template('home.html', menu_items=menu_items, cart_items=cart_items, total_price=total_price)
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/user_management')
+def user_management():
+    name=session.get("name")
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    users_data = cursor.fetchall()
+    connection.close()
+    return render_template('usermgmt.html', users_data=users_data,name=name)
+
+
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Add authentication logic here
-        # For example, check if username and password match a user in the database
-        # If authentication is successful, set the user_id in the session
-        session['user_id'] = 1  # Set user_id to 1 for now, replace with actual user_id
-        return redirect(url_for('home'))
-    return render_template('login.html')
+    if request.method == "POST":
+        user_type_form = request.form.get("user-type")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, role, username FROM users WHERE username=? AND password=?",
+            (username, password),
+        )
+        result = cursor.fetchone()
+
+        if result is not None:
+            user_id, user_type, name = result
+            session["user_id"] = user_id
+            session["user_type"] = user_type
+            session["username"] = username
+            session["name"] = name
+
+            if user_type_form == user_type:
+                return redirect(url_for("home"))
+        error_message = "Invalid username or password"
+        return render_template("error.html", error=error_message)
+
+    return render_template("login.html")
+
+@app.route("/profile")
+def profile():
+    user_id = session.get("user_id")
+    user_type = session.get("user_type")
+
+    if user_id is None:
+        return "User ID not found"
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    if user_type == "customer":
+        cursor.execute("SELECT id, name, dob, email, course, contact, address, pfp FROM studentlist WHERE id=?", (user_id,))
+        user_info = cursor.fetchone()
+        if user_info is None:
+            conn.close()
+            return "User not found"
+
+        id, name, dob, email, course, contact, address, pfp = user_info
+        conn.close()
+        return render_template(
+            "profile.html",
+            id=id,
+            user_type=user_type,
+            name=name,
+            dob=dob,
+            email=email,
+            pfp=pfp,
+            course=course,
+            contact=contact,
+            address=address,
+        )
+    
+    elif user_type == "manager":
+        cursor.execute("SELECT id, name, dob, email, department, contact, address, pfp FROM facultylist WHERE id=?", (user_id,))
+        user_info = cursor.fetchone()
+        if user_info is None:
+            conn.close()
+            return "User not found"
+
+        id, name, dob, email, department, contact, address, pfp = user_info
+        conn.close()
+        return render_template(
+            "profile.html",
+            id=id,
+            user_type=user_type,
+            name=name,
+            dob=dob,
+            email=email,
+            pfp=pfp,
+            course=department,
+            contact=contact,
+            address=address,
+        )
+    elif user_type == "admin":
+        cursor.execute("SELECT id, name, dob, email, department, contact, address, pfp FROM facultylist WHERE id=?", (user_id,))
+        user_info = cursor.fetchone()
+        if user_info is None:
+            conn.close()
+            return "User not found"
+
+        id, name, dob, email, department, contact, address, pfp = user_info
+        conn.close()
+        return render_template(
+            "profile.html",
+            id=id,
+            user_type=user_type,
+            name=name,
+            dob=dob,
+            email=email,
+            pfp=pfp,
+            course=department,
+            contact=contact,
+            address=address,
+        )
+
+    return "User not found"
 
 @app.route('/add_to_cart/<int:item_id>', methods=['POST'])
 def add_to_cart(item_id):
     if request.method == 'POST':
-        current_user = session.get('user_id', 1)  # Get user_id from session or default to 1
+        current_user = session.get('user_id')  
         conn = get_db_connection()
         existing_item = conn.execute('SELECT * FROM Cart WHERE item_id = ? AND user_id = ?', (item_id, current_user)).fetchone()
         if existing_item:
@@ -52,7 +169,7 @@ def add_to_cart(item_id):
 @app.route('/remove_from_cart/<int:item_id>', methods=['POST'])
 def remove_from_cart(item_id):
     if request.method == 'POST':
-        current_user = session.get('user_id', 1)  # Get user_id from session or default to 1
+        current_user = session.get('user_id')  
         conn = get_db_connection()
         conn.execute('DELETE FROM Cart WHERE item_id = ? AND user_id = ?', (item_id, current_user))
         conn.commit()
@@ -61,7 +178,7 @@ def remove_from_cart(item_id):
 
 @app.route('/checkout')
 def checkout():
-    current_user = session.get('user_id', 1)  # Get user_id from session or default to 1
+    current_user = session.get('user_id')  
     conn = get_db_connection()
     conn.execute('DELETE FROM Cart WHERE user_id = ?', (current_user,))
     conn.commit()
--- workarea.py
+++ workarea.py
@@ -6,36 +6,15 @@ def create_users_table():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Menu (
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Cart (
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL UNIQUE,
-                    price real NOT NULL,
-                    image_url text NOT NULL
+                    price TEXT NOT NULL,
+                    quantity TEXT NOT NULL
                     )''')
-
-    conn.commit()
-    conn.close()
-
-def add_users_with_roles():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-
-    users_with_roles = [
-        ('Appam', 50, 'static/images/appam.jpg'),
-        ('Idiyappam', 45, 'static/images/idiyappam.jpg'),
-        ('Puttu', 40, 'static/images/puttu.jpg'),
-        ('Kerala Parotta', 45, 'static/images/porotta.jpg'),
-        ('Thalassery Biriyani', 120, 'static/images/biriyani.jpg'),
-        ('Kerala Fish Curry', 150, 'static/images/fishcurry.jpg'),
-        ('Avial', 80, 'static/images/avial.jpg'),
-        ('Sadya', 200, 'static/images/sadya.jpg')
-    ]
-
-    cursor.executemany("INSERT INTO Menu (name, price, image_url) VALUES (?, ?, ?)", users_with_roles)
 
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
     create_users_table()
-    add_users_with_roles()
