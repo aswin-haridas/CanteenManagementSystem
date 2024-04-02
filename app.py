af87a72761ac6e30d2e59f544f9f939d2f019512
aswin-haridas <aswinharidas0@gmail.com>
2024-04-02 23:35:39 +0530

    commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db_connection():
 @app.route('/')
 def home():
     conn = get_db_connection()
-    menu_cursor = conn.execute('SELECT * FROM Menu')  # Ensure table name is correct
+    menu_cursor = conn.execute('SELECT * FROM Menu')  
     menu_items = menu_cursor.fetchall()
     cart_cursor = conn.execute('SELECT * FROM Cart')
     cart_items = cart_cursor.fetchall()
@@ -22,11 +22,11 @@ def home():
 
 @app.route('/add_to_cart/<int:item_id>', methods=['POST'])
 def add_to_cart(item_id):
-    conn = get_db_connection()  # Use get_db_connection function to connect to the database
+    conn = get_db_connection()  
     item = conn.execute('SELECT name, price FROM Menu WHERE id = ?', (item_id,)).fetchone()
     existing_item = conn.execute('SELECT * FROM Cart WHERE item_id = ?', (item_id,)).fetchone()
     if existing_item:
-        new_quantity = existing_item['quantity'] + 1  # Increment quantity directly
+        new_quantity = existing_item['quantity'] + 1  
         conn.execute('UPDATE Cart SET quantity = ? WHERE item_id = ?', (new_quantity, existing_item['item_id']))
     else:
         conn.execute('INSERT INTO Cart (item_id, item_name, price, quantity) VALUES (?, ?, ?, ?)', (item_id, item['name'], item['price'], 1))
@@ -34,16 +34,16 @@ def add_to_cart(item_id):
     conn.close()
     return redirect(url_for('home'))
 
-@app.route('/remove_from_cart/<string:item_name>', methods=['POST'])  # Changed item_id to item_name
-def remove_from_cart(item_name):  # Changed item_id to item_name
+@app.route('/remove_from_cart/<string:item_name>', methods=['POST'])  
+def remove_from_cart(item_name):  
     conn = get_db_connection()
-    existing_item = conn.execute('SELECT * FROM Cart WHERE item_name = ?', (item_name,)).fetchone()  # Changed item_id to item_name
+    existing_item = conn.execute('SELECT * FROM Cart WHERE item_name = ?', (item_name,)).fetchone()  
     if existing_item:
-        new_quantity = existing_item['quantity'] - 1  # Decrement quantity directly
+        new_quantity = existing_item['quantity'] - 1  
         if new_quantity <= 0:
-            conn.execute('DELETE FROM Cart WHERE item_name = ?', (item_name,))  # Changed item_id to item_name
+            conn.execute('DELETE FROM Cart WHERE item_name = ?', (item_name,))  
         else:
-            conn.execute('UPDATE Cart SET quantity = ? WHERE item_name = ?', (new_quantity, item_name))  # Changed item_id to item_name
+            conn.execute('UPDATE Cart SET quantity = ? WHERE item_name = ?', (new_quantity, item_name))  
         conn.commit()
     conn.close()
     return redirect(url_for('home'))
